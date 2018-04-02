9db31ad43fb414233658e347cd691ff55c743c6d
Denis Defreyne <denis.defreyne@stoneship.org>
2018-04-02 15:47:53 +0200

    Make API_URL configurable

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,8 @@ import AppContainer from './containers/App'
 
 // --- Boot
 
-// TODO: Make baseURL configurable
 ReactDOM.render(
-  <AppContainer baseURL="http://localhost:9292" />,
+  <AppContainer baseURL={process.env.API_URL} />,
   document.getElementById('app')
 )
 

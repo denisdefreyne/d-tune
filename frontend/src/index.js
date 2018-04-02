a9d21087eaf64c284c60c05e863aaf41f5498714
Denis Defreyne <denis.defreyne@stoneship.org>
2018-04-02 21:44:05 +0200

    Cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom'
 
 import AppContainer from './containers/App'
 
-// --- Boot
-
 ReactDOM.render(
   <AppContainer baseURL={process.env.API_URL} />,
   document.getElementById('app')
 )
-
-module.hot.accept()

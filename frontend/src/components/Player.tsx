11ecc7de970df1262113ddba956a9ae2064a90b9
Denis Defreyne <denis.defreyne@stoneship.org>
2018-04-09 22:35:59 +0200

    Add experimental new player

--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+
 import PlayingTrack from "../types/playing_track";
 import { MiniTitle, StatusBox } from "./common";
+import * as style from "./style.css";
 
 interface ActivePlayerProps {
   onPlaybackEnded: (t: PlayingTrack) => void;
@@ -13,21 +15,95 @@ interface LoadedAudioElementProps {
   playingTrack: PlayingTrack;
 }
 
-const LoadedAudioElement = (props: LoadedAudioElementProps) => (
-  <audio
-    style={{ float: "right", width: "500px" }}
-    controls
-    autoPlay
-    onEnded={() => props.onPlaybackEnded(props.playingTrack)}
-    src={props.mediaURL}
-  />
-);
+enum PlayingState {
+  Playing,
+  Paused,
+}
 
-const LoadingAudioElement = () => (
-  <span
-    style={{ float: "right", width: "500px", textAlign: "right", padding: "10px" }}
-  >…</span>
-);
+interface LoadedAudioElementState {
+  elapsedTime: number;
+  totalDuration: number;
+  playingState: PlayingState;
+}
+
+class LoadedAudioElement extends React.Component<LoadedAudioElementProps, LoadedAudioElementState> {
+  private audioRef: HTMLAudioElement | null;
+  private setAudioRef: (elem: HTMLAudioElement) => void;
+
+  constructor(props: LoadedAudioElementProps) {
+    super(props);
+
+    this.state = {
+      elapsedTime: 0,
+      totalDuration: 0,
+      playingState: PlayingState.Paused,
+    };
+
+    this.audioRef = null;
+    this.setAudioRef = (elem: HTMLAudioElement) => {
+      this.audioRef = elem;
+    };
+  }
+
+  public render() {
+    return (
+      <div className={style.player}>
+        <button onClick={() => this.onClickPlay()} className={style.playerControls}>
+          {this.state.playingState === PlayingState.Paused ? "▶" : "❚❚"}
+        </button>
+        <progress max={this.state.totalDuration} value={this.state.elapsedTime} className={style.playerProgress} />
+        <audio
+          autoPlay
+          onTimeUpdate={() => this.onTimeUpdate()}
+          onDurationChange={() => this.onDurationChange()}
+          onPlaying={() => this.onPlaying()}
+          onPause={() => this.onPause()}
+          onEnded={() => this.props.onPlaybackEnded(this.props.playingTrack)}
+          src={this.props.mediaURL}
+          ref={this.setAudioRef}
+        />
+      </div>
+    );
+  }
+
+  private onPlaying() {
+    this.setState({
+      playingState: PlayingState.Playing,
+    });
+  }
+
+  private onPause() {
+    this.setState({
+      playingState: PlayingState.Paused,
+    });
+  }
+
+  private onClickPlay() {
+    if (this.audioRef) {
+      if (this.state.playingState === PlayingState.Paused) {
+        this.audioRef.play();
+      } else {
+        this.audioRef.pause();
+      }
+    }
+  }
+
+  private onDurationChange() {
+    if (this.audioRef) {
+      this.setState({
+        totalDuration: this.audioRef.duration,
+      });
+    }
+  }
+
+  private onTimeUpdate() {
+    if (this.audioRef) {
+      this.setState({
+        elapsedTime: this.audioRef.currentTime,
+      });
+    }
+  }
+}
 
 const ActivePlayer = (props: ActivePlayerProps) => (
   <div>
@@ -38,7 +114,7 @@ const ActivePlayer = (props: ActivePlayerProps) => (
             mediaURL={props.playingTrack.mediaURL}
             playingTrack={props.playingTrack}
           />
-        : <LoadingAudioElement />
+        : null
     }
     <MiniTitle>Now playing</MiniTitle><br />
     {props.playingTrack.track.artist.name} – {props.playingTrack.track.name}

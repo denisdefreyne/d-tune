e8d7eb4c1ba8f66c83d3667dfac0bcae033caaa8
Denis Defreyne <denis.defreyne@stoneship.org>
2018-10-07 09:52:20 +0200

    Remove useless imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,12 +3,6 @@ import * as ReactDOM from "react-dom";
 
 import AppContainer from "./containers/App";
 
-import Album from "./types/album";
-import Artist from "./types/artist";
-import Label from "./types/label";
-import PlayingTrack from "./types/playing_track";
-import Track from "./types/track";
-
 // FIXME: Remove !
 const baseURL = process.env.API_URL!;
 
